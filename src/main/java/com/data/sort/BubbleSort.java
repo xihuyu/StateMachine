714589f731a1d788c70396ba751d7355a56d09e1
fenggaopan <fenggaopan@auteltech.net>
2020-06-17 15:09:26 +0800

    冒泡排序

---
+++ src/main/java/com/data/sort/BubbleSort.java
@@ -0,0 +1,24 @@
+package com.data.sort;
+
+/**
+ * @version V1.0
+ * @description: 冒泡排序
+ * @author: fenggaopan
+ * @date: 2020-06-17 15:08:05
+ */
+public class BubbleSort {
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 冒泡排序
+     * @param bubbleArray 待排序数组
+     * @param length 数组长度
+     * @return 返回排序后的数组
+     */
+    public int[] bubbleSort(int bubbleArray, int length) {
+
+    }
+}
---
+++ src/main/java/com/data/sort/QuickSort.java
@@ -0,0 +1,23 @@
+package com.data.sort;
+
+/**
+ * @version V1.0
+ * @description:
+ * 快速排序：
+ * 1.快速排序是指以某个数为基数
+ * 2.大于基数的放到右边，小于基数的放到左边
+ * 3.分别排序，排序后所有的数据都排序成功
+ * @author: fenggaopan
+ * @date: 2020-06-17 11:24:34
+ */
+public class QuickSort {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int[] quickSort(int sortList[], int length) {
+        return null;
+    }
+
+}
