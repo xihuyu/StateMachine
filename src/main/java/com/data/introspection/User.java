2e274d5de909df0bf0e6b4fec7c5f59f08fb5c60
fenggaopan <fenggaopan@auteltech.net>
2020-07-01 10:33:30 +0800

    jdk内省，反转链表

---
+++ src/main/java/com/data/introspection/User.java
@@ -0,0 +1,29 @@
+package com.data.introspection;
+
+/**
+ * @version V1.0
+ * @description: 用户类
+ * @author: fenggaopan
+ * @date: 2020-07-01 10:16:55
+ */
+public class User {
+
+    private String name;
+    private String idNo ;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIdNo() {
+        return idNo;
+    }
+
+    public void setIdNo(String idNo) {
+        this.idNo = idNo;
+    }
+}
---
+++ src/main/java/com/data/linkedlist/Reverselists.java
@@ -0,0 +1,24 @@
+package com.data.linkedlist;
+
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.util.Collections;
+import java.util.Observable;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicMarkableReference;
+import java.util.concurrent.atomic.AtomicStampedReference;
+
+/**
+ * @version V1.0
+ * @description: 反转单链表
+ * @author: fenggaopan
+ * @date: 2020-06-30 15:44:14
+ */
+public class Reverselists {
+
+    public static void main(String[] args) {
+    }
+}
